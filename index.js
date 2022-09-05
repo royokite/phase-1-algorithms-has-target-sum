be0b750f02e752b9b9189185d39ab046a45f9c65
RoyOkite <royokite@gmail.com>
2022-09-05 22:58:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // Check if two numbers in an array add up to some target number
+  //add each element in the array to all the other elements
+  // return true if a pair exists that adds up to the target value
+  // return false otherwise
+  for(let i = 0; i < array.length; i++) {
+    for(let j = (i + 1); j < array.length; j++) {
+      if(array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  } return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n²)
 */
 
 /* 
   Add your pseudocode here
+  for loop for each element and the remaining to find a valid pair
+  if a pair exists
+    return true
+  else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  loop through the array
+  add each index value with all the rest until the sum matches the target
+  return true if the pair exists
+  return false otherwise
 */
 
 // You can run `node index.js` to view these console logs
